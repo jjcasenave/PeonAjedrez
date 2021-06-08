8ecfda08970c50ff3a96c74261bef91d4c51aa1e
Masenko <juanjosecasenavealonso@gmail.com>
2021-06-08 16:54:03 +0200

    Creados Getters y Setters de clase Peon

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
@@ -9,8 +9,27 @@ public class Peon {
 	//Atributos
 	private Color color;
 	private Posicion posicion;
-	
+		
 	//Metodos
+	
+	//Getters y Setters
+	
+	private void setColor(Color color) {
+		if(color==null)
+			throw new NullPointerException("ERROR: el color no puede ser nulo");
+		else
+			this.color = color;
+	}
+	public Posicion getPosicion() {
+		return new Posicion(posicion);
+	}
+	private void setPosicion(Posicion posicion) {
+		if(posicion==null)
+			throw new NullPointerException("ERROR: la posicion no es valida");
+		else
+			this.posicion = new Posicion(posicion);
+	}
+	
 	
 	
 
