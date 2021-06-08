b5a17c78f5d2b25fa516b5bf6a7f8718161928a6
Masenko <juanjosecasenavealonso@gmail.com>
2021-06-08 18:38:46 +0200

    creado metodo mostrarMenu en Main

--- src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
@@ -5,14 +5,22 @@ public class MainApp {
 	private static Peon peon;
 	
 	public static void main(String[] args) {
-		System.out.println("Programa para aprender a colocar y mover un peón en el tablero de ajedrez");
-		System.out.println("-------------------------------------------------------------------------");
-		
+			
 		System.out.println("Hasta luego Lucas!!!!");
 	}
 	
 	private static void mostrarPeon() {
-		System.out.println(peon);
+		System.out.println(peon);		
+	}
+	
+	private static void mostrarMenu() {
+		System.out.println("Programa para aprender a colocar y mover un peón en el tablero de ajedrez");
+		System.out.println("-------------------------------------------------------------------------");
+		System.out.println("1-Crear Peón por defecto");
+		System.out.println("2-Crear Peón por escogiendo el color");
+		System.out.println("3-Crear Peón escogiendo el color y la columna inicial");
+		System.out.println("4-Mover Peón");
+		System.out.println("0-SALIR");
 		
 	}
 	
