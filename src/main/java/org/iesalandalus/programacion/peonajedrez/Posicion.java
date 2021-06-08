9ce7603b737c96cc91744a597d97ac370d9f9749
Masenko <juanjosecasenavealonso@gmail.com>
2021-06-08 16:23:39 +0200

    creados getters y setters de la clase posicion

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
@@ -8,5 +8,25 @@ public class Posicion
 	
 	//Metodos
 	
-
+	//Getters y Setters
+	
+	public int getFila() {
+		return fila;
+	}
+	private void setFila(int fila) {
+		if(fila<1 || fila>8)
+			throw new IllegalArgumentException("ERROR:");
+		
+		this.fila = fila;
+	}
+	public char getColumna() {
+		return columna;
+	}
+	private void setColumna(char columna) {
+		if(columna<'a' || columna>'h')
+			throw new IllegalArgumentException("ERROR:");
+		
+		this.columna = columna;
+	}
+	
 }
