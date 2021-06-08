9e65b4bb21cf0dfbeba5ac16e18b5fd6d87824ab
Masenko <juanjosecasenavealonso@gmail.com>
2021-06-08 18:26:46 +0200

    creados mover hashcode y toString y pasados los test de clase peon

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
@@ -14,18 +14,42 @@ public class Peon {
 		if (direccion == null) {
 			throw new NullPointerException("ERROR: Mover el peón en una dirección nula no está permitido.");
 		}
-		try {
+		try 
+		{
 			int fila = posicion.getFila();
-			fila = (color == Color.BLANCO) ? fila + 1 : fila - 1;
+			fila = (color == Color.BLANCO) ? fila+1:fila-1;
 			if (direccion == Direccion.IZQUIERDA) {
 				setPosicion(new Posicion(fila, (char)(posicion.getColumna() - 1)));
 			} else {
 				setPosicion(new Posicion(fila, (char)(posicion.getColumna() + 1)));		
 			}
-		} catch (IllegalArgumentException e) {
+		} catch (IllegalArgumentException e) 
+		{
 			throw new OperationNotSupportedException("ERROR: Movimiento no válido.");
 		}
-	}	
+	}
+	
+	public void mover(int pasos) throws OperationNotSupportedException {
+		
+		int fila = posicion.getFila();		
+		if (pasos == 2 && ((fila != 2 && color == Color.BLANCO) || (fila != 7 && color == Color.NEGRO))) {
+			
+			throw new OperationNotSupportedException("ERROR: El peón sólo se puede mover 2 pasos cuando se encuentra en la casilla inicial.");
+			
+		} else if (pasos != 1 && pasos != 2) {
+			throw new OperationNotSupportedException("ERROR: El peón sólo se puede mover 1 o 2 pasos.");
+		}
+		
+		try 
+		{
+			fila = (color == Color.BLANCO) ? fila + pasos : fila - pasos;
+			setPosicion(new Posicion(fila, posicion.getColumna()));
+			
+		} catch (IllegalArgumentException e) 
+		{
+			throw new OperationNotSupportedException("ERROR: Movimiento no válido.");
+		}
+	}
 	
 	//Constructor por defecto
 	
@@ -52,7 +76,7 @@ public class Peon {
 	{
 		if(color==null)
 			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
-		if(!(columna=='c' || columna=='f'))
+		if(!(columna=='a' || columna=='b' || columna=='c' || columna=='d' || columna=='e' || columna=='f' || columna=='g' || columna=='h'))
 			throw new IllegalArgumentException("ERROR: Columna no válida.");		
 		
 		setColor(color);
@@ -82,6 +106,40 @@ public class Peon {
 		else
 			this.posicion = new Posicion(posicion);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		result = prime * result + ((posicion == null) ? 0 : posicion.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Peon other = (Peon) obj;
+		if (color != other.color)
+			return false;
+		if (posicion == null) {
+			if (other.posicion != null)
+				return false;
+		} else if (!posicion.equals(other.posicion))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return posicion + ", color=" + color;
+	}
+	
 	
 	
 	
