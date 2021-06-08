d7887449a0ea9379ba0539f3c0b7cb5f6e75e048
Masenko <juanjosecasenavealonso@gmail.com>
2021-06-08 18:44:37 +0200

    creados metodos elegirColor y elegirColumnaInicial

--- src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.peonajedrez;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class MainApp {
 	
 	private static Peon peon;
@@ -23,5 +25,61 @@ public class MainApp {
 		System.out.println("0-SALIR");
 		
 	}
+	
+	private static int elegirOpcion() {
+		int numero;
+		do {
+			System.out.println("Elige una de estás opciones");
+			numero = Entrada.entero();
+			if (numero < 0 || numero > 4) {
+				System.out.println("Introduce un numero correcto");
+			}
+		} while (numero < 0 || numero > 4);
+		return numero;
+	}
+	
+	private static Color elegirColor() {
+	
+	int colorPeon;
+	Color color = null;
+	do {
+		System.out.println("Elige el color del peon");
+		System.out.println("1.Blanco");
+		System.out.println("2.Negro");
+		colorPeon = Entrada.entero();
+		if (colorPeon< 1 && colorPeon > 2) {
+			System.out.println("Introduce una opcion correcta");
+		}
+	} while (colorPeon < 1 && colorPeon > 2);
+
+	switch (colorPeon) {
+	case 1:
+		color = Color.BLANCO;
+		break;
+
+	case 2:
+		color = Color.NEGRO;
+		break;
+
+	default:
+		break;
+	}
+
+	return color;
+}
+	private static char elegirColumnaInicial() {
+		char columna = 'a';
+		char letraColumna;
+
+		do {
+			System.out.println("Elige la columna inicial");
+			letraColumna = Entrada.caracter();
+			if (letraColumna != 'c' && letraColumna != 'f')
+				System.out.println("ERROR: Introduce una opción correcta");
+		} while (letraColumna != 'c' && letraColumna != 'f');
+		columna = letraColumna;
+		return columna;
+	}
+	
 	
 }
