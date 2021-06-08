267211a716bb9485a740d3f8a392f6c0feb4dbd4
Masenko <juanjosecasenavealonso@gmail.com>
2021-06-08 16:48:59 +0200

    Creada clase peon con sus atributos y visibilidad adecuados

---
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
@@ -0,0 +1,17 @@
+package org.iesalandalus.programacion.peonajedrez;
+
+/*Crea la clase Peon, dentro del paquete adecuado, cuyos atributos serán un color (del tipo enumerado Color)
+ *  y posicion (de la clase Posicion), con la visibilidad adecuada. Haz un commit.
+ */
+
+public class Peon {
+	
+	//Atributos
+	private Color color;
+	private Posicion posicion;
+	
+	//Metodos
+	
+	
+
+}
