4871bd3b8cadecb3d714310a01ddae871fcba1cd
Masenko <juanjosecasenavealonso@gmail.com>
2021-06-08 18:34:25 +0200

    creado metodo mostrarPeon en Main

--- src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
@@ -2,11 +2,18 @@ package org.iesalandalus.programacion.peonajedrez;
 
 public class MainApp {
 	
+	private static Peon peon;
+	
 	public static void main(String[] args) {
 		System.out.println("Programa para aprender a colocar y mover un peón en el tablero de ajedrez");
 		System.out.println("-------------------------------------------------------------------------");
 		
 		System.out.println("Hasta luego Lucas!!!!");
+	}
+	
+	private static void mostrarPeon() {
+		System.out.println(peon);
+		
 	}
 	
 }
