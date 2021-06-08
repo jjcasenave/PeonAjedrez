250c33e0b063c9c24481b1a475d609dd8e1df913
Masenko <juanjosecasenavealonso@gmail.com>
2021-06-08 16:29:21 +0200

    creado constructor con parametros de clase posicion

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
@@ -8,6 +8,25 @@ public class Posicion
 	
 	//Metodos
 	
+	//Constructores
+	
+	//Crea un constructor para esta clase que acepte como parámetros la fila y la columna y 
+	//que los asigne a los atributos si son correctos  y si no lance una excepción del mismo 
+	//tipo que la anterior indicando el problema. Para ello utiliza los métodos set anteriormente 
+	//creados. Haz un commit.
+	
+	public Posicion(int fila, char columna) {
+		
+		if(fila<1 || fila>8)
+			throw new IllegalArgumentException("ERROR:");
+		setFila(fila);
+		
+		if(columna<'a' || columna>'h')
+			throw new IllegalArgumentException("ERROR:");
+		setColumna(columna);
+		
+	}
+	
 	//Getters y Setters
 	
 	public int getFila() {
