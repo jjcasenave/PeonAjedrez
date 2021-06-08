dca63da732d3fe66f43ae73d08709ced08d5cb7c
Masenko <juanjosecasenavealonso@gmail.com>
2021-06-08 16:58:14 +0200

    Creado constructor con parametro color

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
@@ -12,6 +12,27 @@ public class Peon {
 		
 	//Metodos
 	
+	//Constructor por defecto
+	
+	public Peon() throws IllegalArgumentException
+	{
+		color=Color.NEGRO;
+		posicion=new Posicion(7, 'd');
+	}
+	
+	//Constructores con parámetros
+	
+	public Peon(Color color) throws NullPointerException, IllegalArgumentException
+	{
+		if(color==null)
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+		setColor(color);
+		if(color.equals(Color.BLANCO))
+			posicion=new Posicion(2, 'd');
+		else
+			posicion=new Posicion(7, 'd');
+	}
+	
 	//Getters y Setters
 	
 	private void setColor(Color color) {
